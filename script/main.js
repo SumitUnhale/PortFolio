e948f437f7923b7e8ab17f8e3174bf534ffd70ea
SumitUnhale <sumitunhale096@gmail.com>
2025-01-12 22:11:30 +0530

    Initial commit

---
+++ script/main.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const textArray = [
+        "Full Stack Developer    ",
+        "Computer Science Student  ",
+        "A Web Developer           "
+    ];
+    let currentIndex = 0;
+    const textElement = document.getElementById('changingText');
+
+    setInterval(() => {
+        textElement.classList.add('hide');
+        
+        setTimeout(() => {
+            currentIndex = (currentIndex + 1) % textArray.length;
+            console.log(currentIndex);
+            textElement.textContent = textArray[currentIndex];
+            textElement.classList.remove('hide');
+        }, 1000);
+    }, 3000);
+
+    let lastScrollTop = 0;
+    const navbar = document.getElementById('navbar');
+
+    window.addEventListener('scroll', function() {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+            navbar.classList.add('hidden');
+        } else {
+            navbar.classList.remove('hidden');
+        }
+
+        lastScrollTop = scrollTop;
+    });
+
+    const eduBoxes = document.querySelectorAll(".edu-box");
+
+    function checkVisibility() {
+        eduBoxes.forEach((box) => {
+            const boxTop = box.getBoundingClientRect().top;
+            const boxBottom = box.getBoundingClientRect().bottom;
+            const windowHeight = window.innerHeight;
+
+            if (boxTop < windowHeight - 50 && boxBottom > 50) {
+                box.classList.add("active");
+            } else {
+                box.classList.remove("active");
+            }
+        });
+    }
+    window.addEventListener("scroll", checkVisibility);
+    checkVisibility();
+    
+
+});
